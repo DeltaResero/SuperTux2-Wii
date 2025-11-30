be16679d3bd55f31675cca71b3e14eadb936858c
DeltaResero <3397617+DeltaResero@users.noreply.github.com>
2025-11-30 10:17:49 -0400

    build: Remove Travis CI configuration

--- .travis/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-shopt -s nullglob
-
-for file in SuperTux-*; do
-    echo "Uploading $file";
-#    curl -T "$file" -umaths22:$BINTRAY_KEY "https://api.bintray.com/content/supertux/SuperTux-Nightly/travis/$TRAVIS_BUILD_NUMBER/$file?publish=1&override=1"
-done
